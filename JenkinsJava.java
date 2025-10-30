76c54f84edc20f82340d3d96e00949c893300cd5
SaiVarshitha123-marojuvarsh <marojuvarshitha28@gmail.com>
2025-10-30 20:53:01 +0530

    Triggers with webhook second test

--- JenkinsJava.java
+++ JenkinsJava.java
@@ -1,6 +1,6 @@
 public class JenkinsJava{
 	public static void main(String[] args){
-		System.out.println("Hello Jenkins Output with Triggers Testing with Webhook");
+		System.out.println("Hello Jenkins Output with Triggers Testing with Webhook second testing");
 	}
 }
 
