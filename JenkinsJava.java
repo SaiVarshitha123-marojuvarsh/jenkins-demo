5ee5dd9ca0283d43174ce46c02d6e371d551296f
SaiVarshitha123-marojuvarsh <marojuvarshitha28@gmail.com>
2025-10-30 20:48:18 +0530

    newly added jenkins file with webhook

--- JenkinsJava.java
+++ JenkinsJava.java
@@ -1,5 +1,7 @@
 public class JenkinsJava{
 	public static void main(String[] args){
-		System.out.println("Hello Jenkins Output with Triggers Testing");
+		System.out.println("Hello Jenkins Output with Triggers Testing with Webhook");
 	}
 }
+
+
